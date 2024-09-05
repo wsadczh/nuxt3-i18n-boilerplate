9a12a3fc66bf03885868eb2e638138d22b506114
Tarik Kavaz <tarikkavaz@gmail.com>
2024-09-05 10:46:43 +0300

    fr

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer
     class="container mx-auto min-h-20 max-w-screen-xl border-t-[1px] border-slate-500 p-4 text-slate-900 dark:text-slate-100"
   >
-    <div class="mx-auto grid h-full gap-4 md:grid-cols-12 md:gap-8 md:px-8">
+    <div class="grid h-full gap-4 mx-auto md:grid-cols-12 md:gap-8 md:px-8">
       <div class="md:col-span-2">
         <Logo :class="'my-auto h-10 w-auto'" />
       </div>
@@ -16,7 +16,7 @@
           10500 N De Anza Blvd, Cupertino, CA 95014, United States<br /><a
             :href="'mailto:' + email"
             ><EnvelopeIcon
-              class="mr-1 inline-block size-4"
+              class="inline-block mr-1 size-4"
               aria-hidden="true"
             />{{ email }}</a
           >
